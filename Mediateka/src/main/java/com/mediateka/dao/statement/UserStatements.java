fc530537ac5af6de1e27dc07fb4bc6464419961e
oleh_pylypchak <oleh_pylypchak@6cd43515-b9ed-4900-8967-bd84a2012016>
2015-06-11 12:55:47 +0000

    change UserStatements

--- Mediateka/src/main/java/com/mediateka/dao/statement/UserStatements.java
+++ Mediateka/src/main/java/com/mediateka/dao/statement/UserStatements.java
@@ -4,24 +4,24 @@ public class UserStatements {
 	public static final String INSERT_USER = "INSERT INTO user "
 			+ "(form_id, first_name, last_name, middle_name, birth_date,"
 			+ " nationality, education, profession_id, edu_institution,"
-			+ " phone, adress, join_date, email, password, role, state, is_form_active)"
-			+ " VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?) ";
+			+ " phone, adress, join_date, email, password, role, state, is_form_active, salt)"
+			+ " VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?) ";
 	public static final String[] INSERT_USER_ORDER = { "form_id", "first_name",
 			"last_name", "middle_name", "birth_date", "nationality",
 			"education", "profession_id", "edu_institution", "phone", "adress",
-			"join_date", "email", "password", "role", "state" , "is_form_active" };
+			"join_date", "email", "password", "role", "state" , "is_form_active", "salt" };
 
 	public static final String SELECT_USER_BY_ID = "SELECT * FROM user WHERE id =?";
 	public static final String[] SELECT_USER_BY_ID_ORDER = { "id" };
 
 	public static final String UPDATE_USER_BY_ID = "UPDATE user SET form_id=?, first_name=?, last_name=?, middle_name=?, birth_date=?,"
 			+ " nationality=?, education=?, profession_id=?, edu_institution=?,"
-			+ " phone=?, adress=?, join_date=?, email=?, password=?, role=?, state=?, is_form_active=? WHERE id =?";
+			+ " phone=?, adress=?, join_date=?, email=?, password=?, role=?, state=?, is_form_active=?,salt=? WHERE id =?";
 	public static final String[] UPDATE_USER_BY_ID_ORDER = { "form_id",
 			"first_name", "last_name", "middle_name", "birth_date",
 			"nationality", "education", "profession_id", "edu_institution",
 			"phone", "adress", "join_date", "email", "password", "role",
-			"state","is_from_active", "id" };
+			"state","is_from_active","salt", "id" };
 
 	public static final String SELECT_USER_BY_EMAIL = "SELECT * FROM user WHERE email =?";
 	public static final String[] SELECT_USER_BY_EMAIL_ORDER = { "email" };
