4e7f017e74afbd48ea004ebf63fac13919a6fc3c
oleh_pylypchak <oleh_pylypchak@6cd43515-b9ed-4900-8967-bd84a2012016>
2015-06-11 14:09:24 +0000

    Change IndexController

--- Mediateka/src/main/java/com/mediateka/controller/IndexController.java
+++ Mediateka/src/main/java/com/mediateka/controller/IndexController.java
@@ -1,6 +1,7 @@
 package com.mediateka.controller;
 
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -8,12 +9,19 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.mediateka.annotation.Controller;
 import com.mediateka.annotation.Request;
+import com.mediateka.model.User;
+import com.mediateka.util.ObjectFiller;
 
 @Controller
 public class IndexController {
    
 	@Request(url="index", method="get")
 	public static void indexGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
-		request.getRequestDispatcher("index.jsp").forward(request, response);
+		request.getRequestDispatcher("pages/main/main.jsp").forward(request, response);
+	}
+	
+	@Request(url="index", method="post")
+	public static void indexPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException, NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException, InvocationTargetException{
+	
 	}
 }
